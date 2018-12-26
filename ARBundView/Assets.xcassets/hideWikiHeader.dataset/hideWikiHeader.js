41efbb3691cd946933b64663a167cf1067cfb1dd
riggy2013 <davidpenn@gmail.com>
2018-12-26 16:05:04 +0800

    Done w/ wiki page reformat, removed header, page action, related articles sections.

--- ARBundView/Assets.xcassets/hideWikiHeader.dataset/hideWikiHeader.js
+++
@@ -1,4 +0,0 @@
-var styleTag = document.createElement("style");
-styleTag.textContent = '.header-container, .after-post.widget-area {display:none;}';
-document.documentElement.appendChild(styleTag);
-document.getElementById("page-actions").style.display = "none"
--- ARBundView/hideWikiHeader.js
+++ ARBundView/hideWikiHeader.js
@@ -1,5 +1,5 @@
 var styleTag = document.createElement("style");
-styleTag.textContent = '.header-container, .after-post.widget-area {display:none;}';
+styleTag.textContent = '.header-container, .after-post.widget-area {display:none;} \
+.read-more-container, .after-post.widget-area {display:none;} \
+.hlist, .after-post.widget-area {display:none;}';
 document.documentElement.appendChild(styleTag);
-document.getElementById("page-actions").style.display = "none"
-document.body.style.color = "red";
